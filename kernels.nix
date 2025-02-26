65551a171a52780be9c20df6fe9a1d0b3def1fb4
aimixsaka <aimixsaka@gmail.com>
2025-02-26 21:14:04 +0800

    feat: enable manually VFS Hooks

--- kernels.nix
+++ kernels.nix
@@ -28,10 +28,17 @@ _: {
           ];
           kernelPatches = [
             "${sources.wildplus-kernel-patches.src}/69_hide_stuff.patch"
+            # See https://github.com/backslashxx/KernelSU/issues/5
+            "${sources.wildplus-kernel-patches.src}/new_hooks.patch"
           ];
           kernelSrc = sources.oneplus-ace3-lineageos-22_1.src;
           kernelConfig = ''
+            # For dae proxy, though still not working due to other reason...
             CONFIG_BPF_STREAM_PARSER=y
+            # Enable manually VFS Hooks
+            CONFIG_KSU_WITH_KPROBES=n
+            # See 11. in https://gitlab.com/simonpunk/susfs4ksu/-/tree/gki-android14-5.15
+            CONFIG_KSU_SUSFS_SUS_SU=n
           '';
           #oemBootImg = ./boot.img;
         };
